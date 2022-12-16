972b9ec2c970c26033676f9a35070ebeb4ae6c79
Rachel Newby <rachelcnewby@gmail.com>
2022-12-16 12:24:24 +0000

    Additional checks to test #search functionality

--- ut_parent_child_classes/exercise_music/spec/integration_spec.rb
+++ ut_parent_child_classes/exercise_music/spec/integration_spec.rb
@@ -22,5 +22,29 @@ describe "integration" do
       library.add(song_2)
       expect(library.search("sugar")).to eq [song_1]
     end
+
+    it "#search returns the track that matches the keyword" do
+      library = MusicLibrary.new
+      song_1 = Track.new("Cane Sugar", "Glass Animals")
+      song_2 = Track.new("Woman", "Little Simz")
+      song_3 = Track.new("Overload", "Sugarbabes")
+      library.add(song_1)
+      library.add(song_2)
+      library.add(song_3)
+      expect(library.search("sugar")).to eq [song_1, song_3]
+    end
+  end
+
+  context "When keyword is not present in tracks" do
+     it "#search returns empty array" do
+      library = MusicLibrary.new
+      song_1 = Track.new("Cane Sugar", "Glass Animals")
+      song_2 = Track.new("Woman", "Little Simz")
+      song_3 = Track.new("Overload", "Sugarbabes")
+      library.add(song_1)
+      library.add(song_2)
+      library.add(song_3)
+      expect(library.search("boys")).to eq []
+    end
   end
 end
--- ut_parent_child_classes/exercise_music/spec/music_library_spec.rb
+++ ut_parent_child_classes/exercise_music/spec/music_library_spec.rb
@@ -18,7 +18,45 @@ describe MusicLibrary do
       expect(library.all).to eq [song_1, song_2]
     end
   end
+  context "When keyword is present in tracks" do
+    it "returns the track that matches the keyword" do
+      library = MusicLibrary.new
+      song_1 = double :track, matches?: true
+      song_2 = double :track, matches?: true
+      library.add(song_1)
+      library.add(song_2)
+      expect(library.search("sugar")).to eq [song_1, song_2]
+    end
+  end
 
+  context "When keyword is not present in tracks" do
+    it "returns empty array" do
+      library = MusicLibrary.new
+      song_1 = double :track, matches?: false
+      song_2 = double :track, matches?: false
+      library.add(song_1)
+      library.add(song_2)
+      expect(library.search("sugar")).to eq []
+    end
+  end
+
+  context "When keyword is present in some tracks" do
+    it "returns relevant tracks" do
+      library = MusicLibrary.new
+      song_1 = double :track, matches?: true
+      song_2 = double :track, matches?: false
+      song_3 = double :track, matches?: true
+      library.add(song_1)
+      library.add(song_2)
+      library.add(song_3)
+      expect(library.search("sugar")).to eq [song_1, song_3]
+    end
+  end
+end
+
+=begin
+
+This is the syntax when creating a fake class:
   context "When keyword is present in tracks" do
     it "returns the track that matches the keyword" do
       library = MusicLibrary.new
@@ -42,3 +80,5 @@ class FakeTrack_2
     return true
   end
 end
+
+=end
